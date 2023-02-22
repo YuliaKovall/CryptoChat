8c63b62d692531d304f69329dde7f7c95d771051
Roman Koroliuk <avtokoleso@ukr.net>
2023-02-22 23:01:53 +0200

    IG11-43 v1

---
+++ app/src/main/java/com/example/cryptochat/UserAdapter.java
@@ -0,0 +1,66 @@
+package com.example.cryptochat;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cryptochat.databinding.UserItemBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder> {
+    private List<User> userArrayList = new ArrayList<>();
+
+    public UserAdapter() {
+    }
+
+    public UserAdapter(List<User> userArrayList) {
+
+        this.userArrayList = userArrayList;
+    }
+
+    public class UserHolder extends RecyclerView.ViewHolder {
+        private UserItemBinding binding;
+
+        public UserHolder(View view) {
+            super(view);
+            binding = UserItemBinding.bind(view);
+        }
+
+        public void bind(User user) {
+            binding.name.setText(user.getUserName());
+            binding.message.setText(user.getMessage());
+            binding.count.setText(String.valueOf(user.getCount()));
+            binding.time.setText(String.valueOf(user.getTime()));
+        }
+
+    }
+
+    @NonNull
+    @Override
+    public UserHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_item, parent, false);
+        return new UserHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserHolder holder, int position) {
+        holder.bind(userArrayList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return userArrayList.size();
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void addUser(User user) {
+        userArrayList.add(user);
+        notifyDataSetChanged();
+    }
+}
