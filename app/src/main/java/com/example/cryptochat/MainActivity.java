b97c2e765721bc32a73f462b9e7078ec1853e96a
Ivan Stepanchenko <stivax@gmail.com>
2023-02-27 03:38:57 +0200

    animation and chat list

--- app/src/main/java/com/example/cryptochat/MainActivity.java
+++ app/src/main/java/com/example/cryptochat/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+
 import com.example.cryptochat.databinding.ActivityMainBinding;
 
 import java.util.Date;
