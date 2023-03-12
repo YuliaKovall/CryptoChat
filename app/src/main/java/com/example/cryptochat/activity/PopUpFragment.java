25af88acb6c21f7ff7bb0853a82dacacdbbaea25
VolandevlodD <vladradko2000@gmail.com>
2023-03-12 22:59:42 +0200

    Update PopUpFragment.java

--- app/src/main/java/com/example/cryptochat/activity/PopUpFragment.java
+++ app/src/main/java/com/example/cryptochat/activity/PopUpFragment.java
@@ -23,37 +23,84 @@ import com.example.cryptochat.R;
 import com.example.cryptochat.controller.FileController;
 
 public class PopUpFragment extends Dialog {
+
     private final Context mContext;
     private final String contactNumber;
     private String pin;
-    private Drawable buttonDrawable;
 
-    protected PopUpFragment(@NonNull Context context, String contactNumber, boolean cancelable, @Nullable OnCancelListener cancelListener) {
+    private TextView numberOfCharacters;
+    private ConstraintLayout constraintLayout;
+    private int myRedColor;
+    private int myGrey75;
+    private EditText editText;
+    private Button button;
+    private ImageButton imageButton;
+    private ImageView trailingIcon;
+    private ImageView caret;
+    private TextView textViewUnderEditText;
+    private final TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            View.OnClickListener errorOnClick = view -> {
+                if (pin.length() < 16) {
+                    editText.setBackgroundResource(R.drawable.pop_up_edit_text_error);
+                    textViewUnderEditText.setText(R.string.notEnoughCharsError);
+                    textViewUnderEditText.setTextColor(myRedColor);
+                    caret.setVisibility(View.VISIBLE);
+                    trailingIcon.setVisibility(View.VISIBLE);
+                }
+            };
+            pin = editText.getText().toString().trim();
+            numberOfCharacters.setText("* Введено символів " + pin.length());
+            imageButton.setVisibility(View.VISIBLE);
+            imageButton.setOnClickListener(view -> editText.setText(""));
+            caret.setVisibility(View.INVISIBLE);
+            trailingIcon.setVisibility(View.INVISIBLE);
+            textViewUnderEditText.setText(R.string.hintForTheUserAboutTheKey2);
+            editText.setBackgroundResource(R.drawable.pop_up_edit_text);
+
+            Drawable buttonDrawable;
+            if (pin.length() >= 16) {
+                buttonDrawable = ContextCompat.getDrawable(button.getContext(), R.drawable.pop_up_button);
+                button.setBackground(buttonDrawable);
+                button.setOnClickListener(view -> {
+                    FileController.addAndEditContactKeyMap(getContext(), contactNumber, pin);
+                    dismiss();
+                });
+
+                textViewUnderEditText.setTextColor(myGrey75);
+
+            } else {
+                buttonDrawable = ContextCompat.getDrawable(button.getContext(), R.drawable.pop_up_button_disabled);
+                button.setBackground(buttonDrawable);
+                textViewUnderEditText.setTextColor(myGrey75);
+                button.setOnClickListener(errorOnClick);
+                constraintLayout.setOnClickListener(errorOnClick);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+    };
+
+    public PopUpFragment(@NonNull Context context, String contactNumber, boolean cancelable, @Nullable OnCancelListener cancelListener) {
         super(context, cancelable, cancelListener);
         this.contactNumber = contactNumber;
         mContext = context;
-
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //
-        TextView numberOfCharacters;
-        ConstraintLayout constraintLayout;
-        int myRedColor;
-        int myBlackColor;
-        int myGrey75;
-        EditText editText;
-        Button button;
-        ImageButton imageButton;
-        ImageView trailingIcon;
-        ImageView caret;
-        TextView textViewUnderEditText;
-        //
         setContentView(R.layout.pop_up_elements);
         getWindow().setBackgroundDrawableResource(R.drawable.pop_up_background);
-        //
+
         constraintLayout = findViewById(R.id.constraintLayoutPopUp);
         numberOfCharacters = findViewById(R.id.textView5);
         button = findViewById(R.id.button2);
@@ -61,62 +108,15 @@ public class PopUpFragment extends Dialog {
         editText = findViewById(R.id.editText);
         trailingIcon = findViewById(R.id.trailingIcon);
         caret = findViewById(R.id.caret);
-        //
+
         myRedColor = mContext.getResources().getColor(R.color.universal_state_error);
-        myBlackColor = mContext.getResources().getColor(R.color.black);
+        int myBlackColor = mContext.getResources().getColor(R.color.black);
         myGrey75 = mContext.getResources().getColor(R.color.universal_text_grey_75);
         editText.setTextColor(myBlackColor);
         editText.setBackgroundResource(R.drawable.pop_up_edit_text);
         textViewUnderEditText = findViewById(R.id.textView4);
-        //
-        View.OnClickListener errorOnClick = view -> {
-            if (pin.length() < 16) {
-                editText.setBackgroundResource(R.drawable.pop_up_edit_text_error);
-                textViewUnderEditText.setText(R.string.notEnoughCharsError);
-                textViewUnderEditText.setTextColor(myRedColor);
-                caret.setVisibility(View.VISIBLE);
-                trailingIcon.setVisibility(View.VISIBLE);
-            }
-        };
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
 
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                pin = editText.getText().toString().trim();
-                numberOfCharacters.setText("* Введено символів " + pin.length());
-                imageButton.setVisibility(View.VISIBLE);
-                imageButton.setOnClickListener(view -> editText.setText(""));
-                caret.setVisibility(View.INVISIBLE);
-                trailingIcon.setVisibility(View.INVISIBLE);
-                textViewUnderEditText.setText(R.string.hintForTheUserAboutTheKey2);
-                editText.setBackgroundResource(R.drawable.pop_up_edit_text);
+        editText.addTextChangedListener(textWatcher);
 
-                if (pin.length() >= 16) {
-                    buttonDrawable = ContextCompat.getDrawable(button.getContext(), R.drawable.pop_up_button);
-                    button.setBackground(buttonDrawable);
-                    button.setOnClickListener(view -> {
-                        FileController.addAndEditContactKeyMap(getContext(), contactNumber, pin);
-                        dismiss();
-                    });
-                    textViewUnderEditText.setTextColor(myGrey75);
-
-                } else {
-                    buttonDrawable = ContextCompat.getDrawable(button.getContext(), R.drawable.pop_up_button_disabled);
-                    button.setBackground(buttonDrawable);
-                    textViewUnderEditText.setTextColor(myGrey75);
-                    button.setOnClickListener(errorOnClick);
-                    constraintLayout.setOnClickListener(errorOnClick);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-        });
     }
-
 }
