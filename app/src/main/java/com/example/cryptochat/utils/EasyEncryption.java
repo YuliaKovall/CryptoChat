2b4aeb87b45b9d77ee8a6f9d7091e7f15bba47a5
VolandevlodD <vladradko2000@gmail.com>
2023-03-13 15:30:45 +0200

    Update EasyEncryption.java

--- app/src/main/java/com/example/cryptochat/utils/EasyEncryption.java
+++ app/src/main/java/com/example/cryptochat/utils/EasyEncryption.java
@@ -17,23 +17,20 @@ public class EasyEncryption {
     }
 
     private void invert(char[] chars) {
-        int length = chars.length;
-        for (int i = 0; i < length / 2; i++) {
-            char temp = chars[i];
-            chars[i] = chars[length - 1 - i];
-            chars[length - 1 - i] = temp;
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = (char) ~(chars[i]);
         }
     }
 
     public String encrypt(String str) {
         char[] chars = adding(str, password.length() % 2 == 0);
-        invert(chars);
+        invert(chars);// використовуємо результат adding() як вхідний параметр для invert()
         return new String(chars);
     }
 
     public String uncrypt(String str) {
-        char[] chars = adding(str, password.length() % 2 != 0);
-        invert(chars);
-        return new String(chars);
+        char[] chars = str.toCharArray();
+        invert(chars);// використовуємо вхідний параметр str як вхідний параметр для invert()
+        return new String(adding(new String(chars), !(password.length() % 2 == 0)));
     }
 }
